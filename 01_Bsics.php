c6a9f373442a4935259ad1ea5a86cbc8f3e2f1bb
mohdarshad86 <mohdarshad8668@gmail.com>
2022-11-03 02:20:29 +0530

    KNIT US Trip

---
+++ 01_Bsics.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorials</title>
+</head>
+<body>
+    <div class="conainer"></div>
+    This is my first PHP Website
+
+    <?php
+
+echo "Hello world, this printed using PHP";
+//secret algorithm (single line comment)
+
+$variable1 = 34;
+$variable2 = 45;
+
+echo $variable1;
+echo $variable2;
+
+// echo $variable1 + $variable2;
+
+
+    ?>
+    <?php
+    echo "Hello world again"
+    ?>
+</body>
+</html>
